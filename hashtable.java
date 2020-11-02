77a77686cde7fa81d75d900a525ae6f82028181f
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-02 20:30:10 +0530

    Create hashtable.java

---
+++ hashtable.java
@@ -0,0 +1,28 @@
+package Collections;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+
+public class hashtable {
+ public static void main(String[] args) {
+
+  Hashtable<Integer, String> hashtable = new Hashtable<>();
+
+  hashtable.put(1,"A");
+  hashtable.put(2,"B" );
+  hashtable.put(3,"C");
+  System.out.println(hashtable);
+  String value = hashtable.get(1);       
+  System.out.println(value);
+  hashtable.remove(3);            
+  Iterator<Integer> itr = hashtable.keySet().iterator();
+  
+  while(itr.hasNext()) 
+  {
+   Integer key = itr.next();
+   String mappedValue = hashtable.get(key);
+
+   System.out.println("Key: " + key + ", Value: " + mappedValue);
+      }
+     }
+    }
