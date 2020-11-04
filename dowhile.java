9ae54f4295f6662929c21d060586346d388258f7
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-04 18:22:48 +0530

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,17 @@
+package com.classprograms;
+
+public class dowhile {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  int i=1;
+  do {
+   System.out.println("siddu");
+   i++;
+  }
+  while(i<=10);
+  
+  
+  }
+  
+  }
