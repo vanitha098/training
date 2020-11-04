ce82c8b4a2c630b4059fc6e5ce8c82ec4c2275cf
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-04 18:23:39 +0530

    Create forloopstatement.java

---
+++ forloopstatement.java
@@ -0,0 +1,17 @@
+package com.classprograms;
+
+public class forloopstatement {
+
+ public static void main(String[] args) {
+  for(int i=0;i<=10;i++)
+  {
+   System.out.println("siddu");
+  }
+  int sum=0;
+  for(int n=123,rem;n>0;rem=n%10,sum=sum+rem,n=n/10);
+  
+   System.out.println("sum is="+sum);
+   
+   }
+   
+   }
