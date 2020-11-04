f1869bd10f6230cfbbd19db7253f6001536a9f4e
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-04 18:25:29 +0530

    Create nestedforloop.java

---
+++ nestedforloop.java
@@ -0,0 +1,21 @@
+package com.classprograms;
+
+public class nestedforloop {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  System.out.println("Start Exercise");
+  for(int i=1;i<=3;i++)
+  {
+   for(int j=1;j<=3;j++)
+   {
+      System.out.println("push up");
+   }
+   System.out.println("Take 30 sec Rest");
+  }
+  System.out.println("stop Exercise");
+  
+  }
+ 
+ 
+  }
