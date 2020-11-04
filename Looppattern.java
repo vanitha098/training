bac11ed53805c99c242098a30813529d187c588d
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-04 18:20:19 +0530

    Create Looppattern.java

---
+++ Looppattern.java
@@ -0,0 +1,19 @@
+package com.classprograms;
+
+public class Looppattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+		System.out.println();
+    }
+    
+  }
+ 
+}
