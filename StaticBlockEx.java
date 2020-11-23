66a572f4c216295f5863c693684775e31cbd732a
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-23 17:56:57 +0530

    Create StaticBlockEx.java

---
+++ StaticBlockEx.java
@@ -0,0 +1,11 @@
+package com.ex;
+
+public class StaticBlockEx {
+	static {
+		System.out.println("Static Block");
+	}
+	public static void main(String args[]) {
+		System.out.println("Static Method");
+	}
+
+}
