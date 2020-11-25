5fa9ff7bd201b0b3925a73312eeafc1ae0d10fb5
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-25 23:20:59 +0530

    Create PreparedScanner.java

---
+++ PreparedScanner.java
@@ -0,0 +1,39 @@
+package com.org.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class PreparedScanner {
+
+ public static void main(String[] args) throws ClassNotFoundException, SQLException {
+  Class.forName("com.mysql.cj.jdbc.Driver");
+  String url="jdbc:mysql://localhost:3306/motivity";
+  String username="root";
+  String password="root";
+  Connection connection=DriverManager.getConnection(url, username, password);
+  String sql="insert into employee values (?,?,?,?)";
+  PreparedStatement preparedStatement=connection.prepareStatement(sql);
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter Employye Details");
+  int eid=sc.nextInt();
+  String ename=sc.next();
+  int esalary=sc.nextInt();
+  String company_Name=sc.next();
+  preparedStatement.setInt(1, eid);
+  preparedStatement.setString(2, ename);
+  preparedStatement.setInt(3, esalary);
+  preparedStatement.setString(4, company_Name);
+  int x=preparedStatement.executeUpdate();
+  if(x==1)
+  {
+   System.out.println("Record inserted");
+  }
+
+
+ }
+
+}
+
