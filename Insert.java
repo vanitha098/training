a6823aa9d76f8d095c64e6e01e2d2efed58bbf81
vanitha098 <73584807+vanitha098@users.noreply.github.com>
2020-11-25 22:36:06 +0530

    Create Insert.java

---
+++ Insert.java
@@ -0,0 +1,33 @@
+package com.org.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Insert {
+
+ public static void main(String[] args) throws ClassNotFoundException, SQLException {
+  
+ Class.forName("com.mysql.cj.jdbc.Driver");
+ String url="jdbc:mysql://localhost:3306/motivity";
+ String username="root";
+ String password="root";
+ Connection connection=DriverManager.getConnection(url, username, password);
+ if(connection!=null)
+ {
+  System.out.println("Connection established");
+ }
+ Statement st=connection.createStatement();
+ int x=st.executeUpdate("insert into employee values(4,'Kvm',15000,'Motivity') ");
+ if(x!=0)
+ {
+  System.out.println("Record inserted");
+ }
+	
+	
+
+}
+
+}
+
